498c373925442894dbf8b845847bb2474f8d6c57
balfassirou <balfassirou@gmail.com>
2022-01-12 13:06:39 +0000

    site web com

---
+++ resources/templates/front/footer.php
@@ -0,0 +1,28 @@
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; BALFA5SIROU 2021</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+   <!--  <script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+<script>paypal.Buttons().render('body');</script>-->
+
+</body>
+
+</html>
---
+++ resources/templates/front/side_nav.php
@@ -0,0 +1,10 @@
+<div class="col-md-3">
+        <p class="lead">Cisse-commercial</p>
+        <div class="list-group">
+
+		<?php
+			get_categories();
+		?>
+
+        </div>
+</div>
